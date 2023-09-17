b740f9c6c4e5b56eb4ec98d6e1853a24fc25a7a8
Chuan Bai <chuan.bai@gmail.com>
2023-09-17 23:05:28 +0100

    Add task runner

---
+++ run_task.py
@@ -0,0 +1,89 @@
+import logging
+import os
+import sys
+
+root = os.path.dirname(__file__)
+sys.path.append(root)
+
+from chatdev.chat_chain import ChatChain
+from camel.typing import ModelType
+
+model = ModelType.GPT_4
+name = "Research"
+
+with open('task.txt', 'r') as f:
+    task = f.read()
+
+def get_config(company):
+    """
+    return configuration json files for ChatChain
+    user can customize only parts of configuration json files, other files will be left for default
+    Args:
+        company: customized configuration name under CompanyConfig/
+
+    Returns:
+        path to three configuration jsons: [config_path, config_phase_path, config_role_path]
+    """
+    config_dir = os.path.join(root, "CompanyConfig", company)
+    default_config_dir = os.path.join(root, "CompanyConfig", "Default")
+
+    config_files = [
+        "ChatChainConfig.json",
+        "PhaseConfig.json",
+        "RoleConfig.json"
+    ]
+
+    config_paths = []
+
+    for config_file in config_files:
+        company_config_path = os.path.join(config_dir, config_file)
+        default_config_path = os.path.join(default_config_dir, config_file)
+
+        if os.path.exists(company_config_path):
+            config_paths.append(company_config_path)
+        else:
+            config_paths.append(default_config_path)
+
+    return tuple(config_paths)
+
+
+config_path, config_phase_path, config_role_path = get_config('Default')
+chat_chain = ChatChain(config_path=config_path,
+                       config_phase_path=config_phase_path,
+                       config_role_path=config_role_path,
+                       task_prompt=task,
+                       project_name=name,
+                       org_name='CB',
+                       model_type=model)
+
+# ----------------------------------------
+#          Init Log
+# ----------------------------------------
+logging.basicConfig(filename=chat_chain.log_filepath, level=logging.INFO,
+                    format='[%(asctime)s %(levelname)s] %(message)s',
+                    datefmt='%Y-%d-%m %H:%M:%S', encoding="utf-8")
+
+# ----------------------------------------
+#          Pre Processing
+# ----------------------------------------
+
+chat_chain.pre_processing()
+
+# ----------------------------------------
+#          Personnel Recruitment
+# ----------------------------------------
+
+chat_chain.make_recruitment()
+
+# ----------------------------------------
+#          Chat Chain
+# ----------------------------------------
+
+chat_chain.execute_chain()
+
+# ----------------------------------------
+#          Post Processing
+# ----------------------------------------
+
+chat_chain.post_processing()
+
